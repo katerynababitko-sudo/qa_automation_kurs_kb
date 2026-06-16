2302a5501b7fdb3d5b637f90a1c498c7e1f22a38
BIDEKAT <Kateryna.Bidenko@ivoclar.com>
2026-06-16 13:45:08 +0200

    Home work 6_4

---
+++ Lessons/Lesson_6/Home_work_6_4.py
@@ -0,0 +1,9 @@
+initial_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "a", "b", 0.2, 9.5]
+
+list_only_even_numbers = []
+
+for element in initial_list:
+    if isinstance(element, int) and element % 2 == 0:
+        list_only_even_numbers.append(element)
+
+print(f"The new list with only even numbers is: {list_only_even_numbers}")
