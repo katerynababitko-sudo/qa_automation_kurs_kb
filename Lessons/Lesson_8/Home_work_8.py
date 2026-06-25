d2db2168ac407b44849239750c95fd4f759b1a55
BIDEKAT <Kateryna.Bidenko@ivoclar.com>
2026-06-25 12:55:28 +0200

    home work 8

---
+++ Lessons/Lesson_8/Home_work_8.py
@@ -0,0 +1,22 @@
+class Student:
+    def __init__(self, name: str, surname: str, age: int, average_grade: float):
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.average_grade = average_grade
+
+    def set_average_student_grade(self, average_grade: float):
+        self.average_grade = average_grade
+
+
+# Task_1
+
+student = Student("Petro", "Petrenko", 18, 4.5)
+
+print(f"The new Student is added:\n {student.name} {student.surname},\n age: {student.age},\n "
+      f"average grade: {student.average_grade}")
+
+student.set_average_student_grade(4.6)
+
+print(f"The information is updated for Student:\n {student.name} {student.surname},\n age: {student.age},\n "
+      f"new average grade: {student.average_grade}")
